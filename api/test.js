d98f5ecb998fda72baa0a8137e97e51433ca6f94
mchidambaramm0109-ctrl <mchidambaramm0109@gmail.com>
2025-11-26 11:12:57 +0530

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,6 +1,15 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    message: "Vercel endpoint working!",
-    time: new Date().toISOString()
+export default async function handler(req, res) {
+
+  const url = "https://webhook.site/a28cfef0-f55c-4192-b62f-e44a4b5439dd";
+
+  await fetch(url, {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({ 
+      message: "Hi from Vercel",
+      time: new Date().toISOString()
+    })
   });
+
+  res.status(200).json({ status: "Sent to webhook.site" });
 }
